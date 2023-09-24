3ed8aea822016a2371883a312bd9166c373c8bcb
Lindsey Bartges <lrbartges@gmail.com>
2023-09-24 15:16:19 -0700

    title box and button container

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const h2 = document.createElement("h2");
 h2.textContent = "Who is Lindsey, and what is this website?!";
 document.querySelector("body").appendChild(h2);
+const buttonContainer = document.getElementById("button-container");
+const button = document.createElement("button");
+button.textContent = "Click Here";
+button.addEventListener("click", function(){
+    alert("You clicked a button! That's awesome!");
+});
+buttonContainer.appendChild(button);

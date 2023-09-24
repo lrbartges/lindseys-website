a9750d786ad37c4e398a4f7e273990063e22c814
Lindsey Bartges <lrbartges@gmail.com>
2023-09-24 14:01:06 -0700

    meta in html and titles

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Who is Lindsey, and what is this website?!";
 document.querySelector("body").appendChild(h2);

c51308c9b24dafe48a07ae0cf7baa1183fe821d9
Lindsey Bartges <lrbartges@gmail.com>
2023-09-24 18:13:17 -0700

    Draft

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const buttonContainer = document.getElementById("button-container");
 const button = document.createElement("button");
+const showPopupButton = document.getElementById("show-popup");
+const popup = document.getElementById("popup");
+const popupContent = document.getElementById("popup-content");
+const closePopupButton = document.getElementById("close-popup");
 button.textContent = "Want to hear a joke?";
 button.classList.add("hoverable");
 
@@ -16,3 +20,24 @@ button.addEventListener("click", function() {
 });
 
 buttonContainer.appendChild(button);
+
+// Select necessary elements
+
+
+// Event listener to show the popup
+showPopupButton.addEventListener("click", function() {
+    // Change the content of the popup
+    popupContent.innerHTML = `
+        <h2>New Popup Content</h2>
+        <p>This is the updated content of the popup.</p>
+    `;
+    
+    // Show the popup
+    popup.style.display = "block";
+});
+
+// Event listener to close the popup
+closePopupButton.addEventListener("click", function() {
+    // Hide the popup
+    popup.style.display = "none";
+});

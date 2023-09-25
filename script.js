0b14d3a7e0ee2ea6cea336cd521119454fb47419
Lindsey Bartges <lrbartges@gmail.com>
2023-09-24 18:14:53 -0700

    Draft 2

--- script.js
+++ script.js
@@ -1,43 +1,8 @@
 const buttonContainer = document.getElementById("button-container");
 const button = document.createElement("button");
-const showPopupButton = document.getElementById("show-popup");
-const popup = document.getElementById("popup");
-const popupContent = document.getElementById("popup-content");
-const closePopupButton = document.getElementById("close-popup");
 button.textContent = "Want to hear a joke?";
-button.classList.add("hoverable");
-
-button.addEventListener("mouseenter", function() {
-    button.textContent = "Why don't programmers like nature?";
-});
-
-button.addEventListener("mouseleave", function() {
-    button.textContent = "Want to hear a joke?";
-});
-
-button.addEventListener("click", function() {
+button:hover.textContent ="Why dont programmers like nature?"
+button.addEventListener("click", function(){
     alert("Because it has too many bugs!");
 });
-
 buttonContainer.appendChild(button);
-
-// Select necessary elements
-
-
-// Event listener to show the popup
-showPopupButton.addEventListener("click", function() {
-    // Change the content of the popup
-    popupContent.innerHTML = `
-        <h2>New Popup Content</h2>
-        <p>This is the updated content of the popup.</p>
-    `;
-    
-    // Show the popup
-    popup.style.display = "block";
-});
-
-// Event listener to close the popup
-closePopupButton.addEventListener("click", function() {
-    // Hide the popup
-    popup.style.display = "none";
-});

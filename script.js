927db19c3cd69d488272a5dda333624f6f7430d0
Lindsey Bartges <lrbartges@gmail.com>
2023-09-24 18:17:41 -0700

    Draft 3

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const buttonContainer = document.getElementById("button-container");
 const button = document.createElement("button");
 button.textContent = "Want to hear a joke?";
-button:hover.textContent ="Why dont programmers like nature?"
-button.addEventListener("click", function(){
+
+button.addEventListener("mouseenter", function() {
+    button.textContent = "Why don't programmers like nature?";
+});
+
+button.addEventListener("mouseleave", function() {
+    button.textContent = "Want to hear a joke?";
+});
+
+button.addEventListener("click", function() {
     alert("Because it has too many bugs!");
 });
+
 buttonContainer.appendChild(button);
